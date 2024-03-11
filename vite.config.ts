36c2163f3021cde413ab9557bb07de4c21fbfa59
Isaac Liu <xnscdev@gmail.com>
2024-03-11 11:30:06 -0700

    Split node_modules code into separate rollup chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules/.pnpm")) {
+            return id.split("node_modules/.pnpm/")[1].split("/")[0];
+          }
+        },
+      },
+    },
+  },
+});
